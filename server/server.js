e8665f90c9bb56b7eb75b175b483dbf5de251d08
Hoangs0 <hoang_lp_97@yahoo.com>
2017-12-11 21:47:55 +0200

    modify server

--- server/server.js
+++ server/server.js
@@ -4,4 +4,10 @@ const app = express();
 
 app.use(express.static(__dirname +'./../public'));
 
+app.get('/',function(req,res){
+    
+  res.sendFile('index.html');
+
+});
+
 app.listen(process.env.PORT || 3000);
